c0267595258c597c5be7f6773fddca05a503cf4e
Sam Sly <samsly7@gmail.com>
2021-05-17 15:17:01 -0600

    DELETE request lesson

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,20 @@
-import { useParams } from "react-router";
+import { useHistory, useParams } from "react-router";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     
     const { id } = useParams();
     const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
-    
+    const history = useHistory();
+
+    const handleClick = () => {
+        fetch('http://localhost:8000/blogs/' + blog.id, {
+            method: 'DELETE'
+        }).then(() => {
+            history.push('/');
+        })
+    }
+
     return ( 
         <div className="blog-details">
             {isPending && <div>Loading...</div>}
@@ -15,6 +24,7 @@ const BlogDetails = () => {
                     <h2>{ blog.title }</h2>
                     <p>Written by { blog.author }</p>
                     <div>{ blog.body }</div>
+                    <button onClick={handleClick}>Delete</button>
                 </article>
             )}
         </div>
