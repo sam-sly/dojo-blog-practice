c7e37bd989d4f4e8e75952eb3ba15330e3a7147e
Sam Sly <samsly7@gmail.com>
2021-05-17 12:53:14 -0600

    Props and Reusable components lesson

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,17 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
 
-    //let name = 'Sam';
-    const [name, setName] = useState('Sam');
-    const [age, setAge] = useState(26);
-
-    const handleClick = () => {
-        setName('Coop');
-        setAge(30);
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
 
     return ( 
         <div className="home">
-            <h2>Home Page</h2>
-            <p>{ name } is { age } years old</p>
-            <button onClick={handleClick}>Click me</button>
+            <BlogList blogs={blogs} title="All Blogs!" />
         </div>
      );
 }
